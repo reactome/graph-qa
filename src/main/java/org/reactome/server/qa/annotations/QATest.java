250ff4edc222372f53a2c19ead300ebe1e7bc63b
Antonio Fabregat <fabregat.antonio@gmail.com>
2016-08-01 16:14:50 +0100

    annotation moved a specific package

--- src/main/java/org/reactome/server/qa/annotations/QATest.java
+++ src/main/java/org/reactome/server/qa/annotations/QATest.java
@@ -1,4 +1,4 @@
-package org.reactome.server.qa;
+package org.reactome.server.qa.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -6,10 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Created by:
- *
  * @author Florian Korninger (florian.korninger@ebi.ac.uk)
- * @since 01.03.16.
  */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
