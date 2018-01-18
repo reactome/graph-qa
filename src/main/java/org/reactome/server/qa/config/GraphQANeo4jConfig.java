738efba87a2b79d6b45baa3259ddc404b4f88500
Antonio Fabregat <fabregat.antonio@gmail.com>
2018-01-18 12:18:40 +0000

    minor change

--- src/main/java/org/reactome/server/qa/config/GraphQANeo4jConfig.java
+++ src/main/java/org/reactome/server/qa/config/GraphQANeo4jConfig.java
@@ -1,15 +1,12 @@
 package org.reactome.server.qa.config;
 
 import org.neo4j.ogm.config.Configuration;
-import org.neo4j.ogm.session.Session;
 import org.neo4j.ogm.session.SessionFactory;
 import org.reactome.server.graph.config.Neo4jConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -27,7 +24,6 @@ public class GraphQANeo4jConfig extends Neo4jConfig {
     private static final Logger logger = LoggerFactory.getLogger("importLogger");
 
     private SessionFactory sessionFactory;
-    private Session session;
 
     @Bean
     public Configuration getConfiguration() {
@@ -48,17 +44,4 @@ public class GraphQANeo4jConfig extends Neo4jConfig {
         }
         return sessionFactory;
     }
-
-    @Override
-    @Bean
-    @Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
-    public Session getSession() throws Exception {
-        if (session == null){
-            logger.info("Opening neo4j Session");
-            session = super.getSession();
-        }
-
-        return session;
-    }
-
 }
