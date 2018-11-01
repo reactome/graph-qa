81af2e5a5542510528c093550b058e13d93e03e4
Antonio Fabregat <fabregat.antonio@gmail.com>
2018-11-01 14:29:20 +0000

    specific sorting established

--- src/main/java/org/reactome/server/qa/tests/T036_EventsWithoutCompartment.java
+++ src/main/java/org/reactome/server/qa/tests/T036_EventsWithoutCompartment.java
@@ -25,16 +25,24 @@ public class T036_EventsWithoutCompartment extends QualityAssuranceAbstract {
 
     @Override
     public List<String> getHeader() {
-        return Arrays.asList("dbId", "stId", "Name", "Created", "Modified");
+        return Arrays.asList("dbId", "stId", "Name", "Type", "Created", "Modified");
     }
 
     @Override
     String getQuery() {
-        return " MATCH (e:Event) " +
+        //The reason why of the splitting is to promote reactions on the top of the list. Sorting by "Type DESC" didn't work because of the TopLevelPathway
+        return " MATCH (e:ReactionLikeEvent) " +
                 "WHERE NOT (e)-[:compartment]->(:Compartment) " +
                 "OPTIONAL MATCH (a)-[:created]->(e) " +
                 "OPTIONAL MATCH (m)-[:modified]->(e) " +
-                "RETURN e.dbId AS dbId, e.stId AS stId, e.displayName AS Name, a.displayName AS Created, m.displayName AS Modified " +
-                "ORDER BY Created, Modified, dbId";
+                "RETURN e.dbId AS dbId, e.stId AS stId, e.displayName AS Name, e.schemaClass AS Type, a.displayName AS Created, m.displayName AS Modified " +
+                "ORDER BY Created, Modified, stId " +
+                "UNION " +
+                "MATCH (e:Pathway) " +
+                "WHERE NOT (e)-[:compartment]->(:Compartment) " +
+                "OPTIONAL MATCH (a)-[:created]->(e) " +
+                "OPTIONAL MATCH (m)-[:modified]->(e) " +
+                "RETURN e.dbId AS dbId, e.stId AS stId, e.displayName AS Name, e.schemaClass AS Type, a.displayName AS Created, m.displayName AS Modified " +
+                "ORDER BY Created, Modified, stId " ;
     }
 }
